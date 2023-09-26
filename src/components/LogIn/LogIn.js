637cb6f4c326088e3b16133e57306cf56022047a
Agustina Mollea <guchimo@Arch.guchibook>
2023-09-26 11:47:43 -0300

    SAS-42 added login view

---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import SmartToyIcon from '@mui/icons-material/SmartToy';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import CasinoIcon from '@mui/icons-material/Casino';
+import Button from '@mui/material/Button';
+
+const defaultTheme = createTheme();
+
+const centerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '90vh',
+};
+
+export default function LogIn() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div style={centerStyle}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 1,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <SmartToyIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Stay Away!
+            </Typography>
+          </Box>
+        </Container>
+
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 6,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Paper
+              component="form"
+              sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+            >
+              <IconButton sx={{ p: '10px' }} aria-label="random name">
+                <CasinoIcon />
+              </IconButton>
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Username"
+                inputProps={{ 'aria-label': 'username' }}
+              />
+            </Paper>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Play
+            </Button>
+          </Box>
+        </Container>
+      </div>
+    </ThemeProvider>
+  );
+}
--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -1,5 +1,29 @@
-const Home = () => {
-  return <h1>Register</h1>;
+import LogIn from "../../components/LogIn/LogIn";
+
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
+const Register = () => {
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box sx={{ flexGrow: 1, maxWidth: 800 }}>
+          <Grid container spacing={20}>
+            <Grid item xs={6} md={20}>
+              <LogIn/>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default Register;
