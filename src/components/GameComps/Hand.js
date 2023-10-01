0d8c0f2e81d94e232ab8486bb4ab8ac411f70460
Igna Martinez <itsigna02@gmail.com>
2023-10-01 00:40:26 -0300

    add design of game participants and place them around the table

--- src/components/GameComps/Hand.js
+++ src/components/GameComps/Hand.js
@@ -3,85 +3,85 @@ import card1 from "../GameComps/Cards/0B.png";
 import card2 from "../GameComps/Cards/0G.png";
 import card3 from "../GameComps/Cards/0R.png";
 import card4 from "../GameComps/Cards/0Y.png";
-import { useState } from 'react';
+import { useState } from "react";
 
 const Hand = () => {
-    const [selectedCard, setSelectedCard] = useState(null);
+  const [selectedCard, setSelectedCard] = useState(null);
 
-    const containerStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    };
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
 
-    const baseCardStyle = {
-        width: '10%',
-        height: 'auto',
-        position: 'relative',
-        top: '450px',
-        userSelect: 'none', 
-        cursor: 'pointer', 
-        border: 'none', 
-    };
+  const baseCardStyle = {
+    width: "10%",
+    height: "auto",
+    position: "relative",
+    top: "220px",
+    userSelect: "none",
+    cursor: "pointer",
+    border: "none",
+  };
 
-    const highlightedCardStyle = {
-        border: '1px groove', 
-    };
+  const highlightedCardStyle = {
+    border: "1px groove",
+  };
 
-    const handleCardClick = (cardId) => {
-        if (selectedCard === cardId) {
-            setSelectedCard(null);
-        } else {
-            setSelectedCard(cardId);
-        }
-    };
+  const handleCardClick = (cardId) => {
+    if (selectedCard === cardId) {
+      setSelectedCard(null);
+    } else {
+      setSelectedCard(cardId);
+    }
+  };
 
-    return (
-        <div>
-            <div style={containerStyle}>
-                <img
-                    src={card1}
-                    alt="card1"
-                    style={{
-                        ...baseCardStyle,
-                        ...(selectedCard === 'card1' ? highlightedCardStyle : {}), 
-                        left: '10px', 
-                    }}
-                    onClick={() => handleCardClick('card1')} 
-                />
-                <img
-                    src={card2}
-                    alt="card2"
-                    style={{
-                        ...baseCardStyle,
-                        ...(selectedCard === 'card2' ? highlightedCardStyle : {}), 
-                        right: '20px', 
-                    }}
-                    onClick={() => handleCardClick('card2')} 
-                />
-                <img
-                    src={card3}
-                    alt="card3"
-                    style={{
-                        ...baseCardStyle,
-                        ...(selectedCard === 'card3' ? highlightedCardStyle : {}), 
-                        right: '50px', 
-                    }}
-                    onClick={() => handleCardClick('card3')} 
-                />
-                <img
-                    src={card4}
-                    alt="card4"
-                    style={{
-                        ...baseCardStyle,
-                        ...(selectedCard === 'card4' ? highlightedCardStyle : {}), 
-                        right: '80px', 
-                    }}
-                    onClick={() => handleCardClick('card4')}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div style={containerStyle}>
+        <img
+          src={card1}
+          alt="card1"
+          style={{
+            ...baseCardStyle,
+            ...(selectedCard === "card1" ? highlightedCardStyle : {}),
+            left: "10px",
+          }}
+          onClick={() => handleCardClick("card1")}
+        />
+        <img
+          src={card2}
+          alt="card2"
+          style={{
+            ...baseCardStyle,
+            ...(selectedCard === "card2" ? highlightedCardStyle : {}),
+            right: "20px",
+          }}
+          onClick={() => handleCardClick("card2")}
+        />
+        <img
+          src={card3}
+          alt="card3"
+          style={{
+            ...baseCardStyle,
+            ...(selectedCard === "card3" ? highlightedCardStyle : {}),
+            right: "50px",
+          }}
+          onClick={() => handleCardClick("card3")}
+        />
+        <img
+          src={card4}
+          alt="card4"
+          style={{
+            ...baseCardStyle,
+            ...(selectedCard === "card4" ? highlightedCardStyle : {}),
+            right: "80px",
+          }}
+          onClick={() => handleCardClick("card4")}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Hand;
--- src/views/Game/Game.js
+++ src/views/Game/Game.js
@@ -1,23 +1,111 @@
 import Hand from "../../components/GameComps/Hand";
+import UserAvatar from "../../components/UserAvatar/UserAvatar";
+import "./Game.css";
 
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
 const Game = () => {
+  const [players, setPLayers] = useState([]);
+
+  let players_example = [
+    { name: "Player 2" },
+    { name: "Player 2" },
+    { name: "Player 3" },
+    { name: "Player 4" },
+    { name: "Player 5" },
+    { name: "Player 6" },
+    { name: "Player 7" },
+    { name: "Player 8" },
+    { name: "Player 9" },
+    { name: "Player 10" },
+    { name: "Player 11" },
+  ];
+
+  const buildCircle = (cnt_players) => {
+    const num = cnt_players; //Number of Square to be generate
+    const type = 1;
+    let radius = "415"; //distance from center
+    let start = 169; //shift start from 0
+    let slice;
+
+    switch (num) {
+      case 4:
+        slice = (185 * type) / num;
+        start = 200;
+        break;
+      case 5:
+        slice = (200 * type) / num;
+        start = 190;
+        break;
+      case 6:
+        slice = (239 * type) / num;
+        break;
+      case 7:
+        slice = (236 * type) / num;
+        break;
+      case 8:
+        slice = (230 * type) / num;
+        break;
+      case 9:
+        slice = (227 * type) / num;
+        break;
+      case 10:
+        slice = (224 * type) / num;
+        break;
+      case 11:
+        slice = (222 * type) / num;
+        break;
+      default:
+        slice = (220 * type) / num;
+        break;
+    }
+
+    let items = [];
+    let i;
+    for (i = 0; i < num; i++) {
+      let rotate = slice * i + start;
+      let rotateReverse = rotate * -1;
+
+      items.push({
+        radius: radius,
+        rotate: rotate,
+        rotateReverse: rotateReverse,
+        name: players_example[i].name,
+      });
+    }
+    setPLayers(items);
+  };
+
+  useEffect(() => {
+    // requests here
+    // hay q ver como hacer, si setter la lista de jugadores a un arreglo (como players_example) o a un estado
+    // (con estados no me funca)
+
+    buildCircle(8); // pass the number of players here
+  }, []);
+
   return (
     <div>
       <div
         style={{
-          display: "flex",
+          display: "column",
           alignItems: "center",
           justifyContent: "center",
         }}
       >
-        <Box sx={{ flexGrow: 1, maxWidth: 800 }}>
+        <div className="circle">
+          <div className="circle-hold">
+            {players.map(function (value, index) {
+              return <UserAvatar css={value} name={value.name} />;
+            })}
+          </div>
+        </div>
+        <Box>
           <Grid container spacing={20}>
-            <Grid item xs={6} md={20}>
-              <Hand/>
+            <Grid item xs={6} md={30}>
+              <Hand />
             </Grid>
           </Grid>
         </Box>
