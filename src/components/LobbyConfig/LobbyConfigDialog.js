10b9e7442e22d353e274d64ccbaf4fd697eea45e
gasmint <gastonbonfils@mi.unc.edu.ar>
2023-09-30 22:56:21 -0300

    SAS-108 structure for post, not complete at all

--- src/components/LobbyConfig/LobbyConfigDialog.js
+++ src/components/LobbyConfig/LobbyConfigDialog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
@@ -8,23 +8,38 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 
+import axios from "axios";
+
 function ValidForm (name) {
   return name.length > 3;
 }
 
 export default function CreateRoomDialog({ open, onClose }) {
-  const [roomName, setRoomName] = React.useState('');
+  const [name, setName] = useState('');
+  
+  const handleCreateRoom = async (event) => {
+    event.preventDefault();
 
-  const [name, setName] = React.useState('');
-    const handleCreateRoom = (event) => {
-      event.preventDefault();
-      const data = { name};
-      console.log(data);
-      //close the dialog
-      onClose();
-      //in the future we will send the data to the server
-      //and the server will create the room
-    };
+    //base url, should be changed to the API URL constant
+    const url = "http://localhost:8000/rooms";
+
+    //build de url with the params
+    const params = "?name=" + name + "&host_id=" + 1; 
+    const urlFinal = url + params;
+
+    axios.post(urlFinal)
+    .then((response) => {
+      console.log('Solicitud POST exitosa', response.data);
+      // Puedes realizar otras acciones después de la solicitud exitosa
+    })
+    .catch((error) => {
+      console.error('Error en la solicitud POST', error);
+      // Puedes manejar el error de alguna manera
+    });
+
+    onClose();
+  };
+    
   
     return (
       <Dialog 
