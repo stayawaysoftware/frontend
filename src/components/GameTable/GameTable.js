cea5b48139a9e222337263afe14c99622b13819e
Igna Martinez <itsigna02@gmail.com>
2023-10-02 19:30:16 -0300

    fix infinite render on game view

--- src/components/GameTable/GameTable.js
+++ src/components/GameTable/GameTable.js
@@ -4,28 +4,12 @@ import { UserContext } from "../../contexts/UserContext";
 
 import React, { useEffect, useState, useContext } from "react";
 
-const GameTable = () => {
+const GameTable = ({ players_example }) => {
   const { userid } = useContext(UserContext);
   const [players, setPLayers] = useState([]);
-  const [orderedPlayers, setOrderedPLayers] = useState([]);
 
-  let players_example = [
-    { id: 1, name: "Player 1", death: false },
-    { id: 2, name: "Player 2", death: false },
-    { id: 3, name: "Player 3", death: true },
-    { id: 4, name: "Player 4", death: false },
-    { id: 5, name: "Player 5", death: false },
-    { id: 6, name: "Player 6", death: false },
-    { id: 7, name: "Player 7", death: false },
-    { id: 8, name: "Player 8", death: true },
-    { id: 9, name: "Player 9", death: false },
-    { id: 10, name: "Player 10", death: false },
-    { id: 11, name: "Player 11", death: false },
-    { id: 12, name: "Player 12", death: false },
-  ];
-
-  const buildCircle = (cnt_players) => {
-    const num = cnt_players; //Number of Square to be generate
+  const buildCircle = (players) => {
+    const num = players.length; //Number of Square to be generate
     const type = 1;
     let radius = "415"; //distance from center
     let start = 169; //shift start from 0
@@ -73,39 +57,36 @@ const GameTable = () => {
         radius: radius,
         rotate: rotate,
         rotateReverse: rotateReverse,
-        name: orderedPlayers[i].name,
-        death: orderedPlayers[i].death,
-        turn: orderedPlayers[i].id === 4, // si es el turno del usuario
+        name: players[i].name,
+        death: players[i].death,
+        turn: players[i].id === 4, // si es el turno del usuario
       });
     }
     setPLayers(items);
   };
 
-  // dependiendo el user id ordenar el arreglo de players
-  const sortPlayers = () => {
-    let sorted_players = [];
-    let i;
-    for (i = 1; i < players_example.length; i++) {
-      if (players_example[i].id === userid) {
-        // sorted_players.push(players_example[i]);
-        break;
-      }
-    }
-    for (let j = i - 1; j >= 0; --j) {
-      if (players_example[j] === undefined) console.log("undefined " + j);
-      sorted_players.push(players_example[j]);
-    }
-    for (let j = players_example.length - 1; j > i; --j) {
-      if (players_example[j] === undefined) console.log("undefined " + j);
-      sorted_players.push(players_example[j]);
-    }
-    setOrderedPLayers(sorted_players);
-  };
-
   useEffect(() => {
+    const sortPlayers = () => {
+      let sorted_players = [];
+      let i;
+      for (i = 1; i < players_example.length; i++) {
+        if (players_example[i].id === userid) {
+          // sorted_players.push(players_example[i]);
+          break;
+        }
+      }
+      for (let j = i - 1; j >= 0; --j) {
+        if (players_example[j] === undefined) console.log("undefined " + j);
+        sorted_players.push(players_example[j]);
+      }
+      for (let j = players_example.length - 1; j > i; --j) {
+        if (players_example[j] === undefined) console.log("undefined " + j);
+        sorted_players.push(players_example[j]);
+      }
+      buildCircle(sorted_players);
+    };
     sortPlayers();
-    if (orderedPlayers.length > 0) buildCircle(orderedPlayers.length);
-  }, [orderedPlayers]);
+  }, []);
 
   return (
     <div className="circle">
--- src/views/Game/Game.js
+++ src/views/Game/Game.js
@@ -14,6 +14,21 @@ const cardList = [200, 201, 202, 203, 3];
 const Game = () => {
   const { gameId } = useParams();
 
+  let players_example = [
+    { id: 1, name: "Player 1", death: false },
+    { id: 2, name: "Player 2", death: false },
+    { id: 3, name: "Player 3", death: true },
+    { id: 4, name: "Player 4", death: false },
+    { id: 5, name: "Player 5", death: false },
+    { id: 6, name: "Player 6", death: false },
+    { id: 7, name: "Player 7", death: false },
+    { id: 8, name: "Player 8", death: true },
+    { id: 9, name: "Player 9", death: false },
+    { id: 10, name: "Player 10", death: false },
+    { id: 11, name: "Player 11", death: false },
+    { id: 12, name: "Player 12", death: false },
+  ];
+
   return (
     <div>
       <div
@@ -31,7 +46,7 @@ const Game = () => {
           backgroundSize: "cover",
         }}
       >
-        <GameTable />
+        <GameTable players_example={players_example} />
         <Box>
           <Grid container spacing={2}>
             <Grid item xs={6} md={20}>
@@ -45,10 +60,9 @@ const Game = () => {
               transform: "translate(16%, 0%)",
               marginBottom: "-70%",
             }}
-          >
-          </div>
+          ></div>
         </Box>
-        <Box>  
+        <Box>
           <Grid container spacing={2}>
             <Grid item xs={6} md={20}>
               <Hand cardList={cardList} />
@@ -62,12 +76,12 @@ const Game = () => {
               marginTop: 0,
             }}
           >
-            <Buttons current_player={5}/>
+            <Buttons current_player={5} />
           </div>
         </Box>
       </div>
     </div>
-    );
-  };
+  );
+};
 
-  export default Game;
+export default Game;
