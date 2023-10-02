b117d657f31391ee099e278924c6462fadbf50da
Igna Martinez <itsigna02@gmail.com>
2023-10-02 16:00:28 -0300

    SAS-168 order position of player with data example and add death avatar

--- src/components/GameTable/GameTable.js
+++ src/components/GameTable/GameTable.js
@@ -1,23 +1,27 @@
 import UserAvatar from "../UserAvatar/UserAvatar";
 import "./GameTable.css";
+import { UserContext } from "../../contexts/UserContext";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 
 const GameTable = () => {
+  const { userid } = useContext(UserContext);
   const [players, setPLayers] = useState([]);
+  const [orderedPlayers, setOrderedPLayers] = useState([]);
 
   let players_example = [
-    { name: "Player 2" },
-    { name: "Player 2" },
-    { name: "Player 3" },
-    { name: "Player 4" },
-    { name: "Player 5" },
-    { name: "Player 6" },
-    { name: "Player 7" },
-    { name: "Player 8" },
-    { name: "Player 9" },
-    { name: "Player 10" },
-    { name: "Player 11" },
+    { id: 1, name: "Player 1", death: false },
+    { id: 2, name: "Player 2", death: false },
+    { id: 3, name: "Player 3", death: true },
+    { id: 4, name: "Player 4", death: false },
+    { id: 5, name: "Player 5", death: false },
+    { id: 6, name: "Player 6", death: false },
+    { id: 7, name: "Player 7", death: false },
+    { id: 8, name: "Player 8", death: true },
+    { id: 9, name: "Player 9", death: false },
+    { id: 10, name: "Player 10", death: false },
+    { id: 11, name: "Player 11", death: false },
+    { id: 12, name: "Player 12", death: false },
   ];
 
   const buildCircle = (cnt_players) => {
@@ -69,27 +73,55 @@ const GameTable = () => {
         radius: radius,
         rotate: rotate,
         rotateReverse: rotateReverse,
-        name: players_example[i].name,
+        name: orderedPlayers[i].name,
+        death: orderedPlayers[i].death,
       });
     }
     setPLayers(items);
   };
 
-  useEffect(() => {
-    // requests here
-    // hay q ver como hacer, si setter la lista de jugadores a un arreglo (como players_example) o a un estado
-    // (con estados no me funca)
+  // dependiendo el user id ordenar el arreglo de players
+  const sortPlayers = () => {
+    let sorted_players = [];
+    let i;
+    for (i = 1; i < players_example.length; i++) {
+      if (players_example[i].id === userid) {
+        // sorted_players.push(players_example[i]);
+        break;
+      }
+    }
+    for (let j = i - 1; j >= 0; --j) {
+      if (players_example[j] === undefined) console.log("undefined " + j);
+      sorted_players.push(players_example[j]);
+    }
+    for (let j = players_example.length - 1; j > i; --j) {
+      if (players_example[j] === undefined) console.log("undefined " + j);
+      sorted_players.push(players_example[j]);
+    }
+    setOrderedPLayers(sorted_players);
+  };
 
-    buildCircle(8); // pass the number of players here
-  }, []);
+  useEffect(() => {
+    sortPlayers();
+    if (orderedPlayers.length > 0) buildCircle(orderedPlayers.length);
+  }, [orderedPlayers]);
 
   return (
     <div className="circle">
-      <div className="circle-hold">
-        {players.map(function (value, index) {
-          return <UserAvatar css={value} name={value.name} key={index} />;
-        })}
-      </div>
+      {players.length ? (
+        <div className="circle-hold">
+          {players.map(function (value, index) {
+            return (
+              <UserAvatar
+                css={value}
+                name={value.name}
+                key={index}
+                death={value.death}
+              />
+            );
+          })}
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/components/UserAvatar/UserAvatar.js
+++ src/components/UserAvatar/UserAvatar.js
@@ -2,8 +2,10 @@ import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import { Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { pink } from "@mui/material/colors";
 
-export default function UserAvatar({ name, css }) {
+export default function UserAvatar({ name, css, death }) {
   return (
     <Stack
       direction="column"
@@ -24,10 +26,39 @@ export default function UserAvatar({ name, css }) {
           "deg)",
       }}
     >
-      <Avatar src="/broken-image.jpg" sx={{ width: 50, height: 50 }} />
-      <Typography variant="h7" component="div" gutterBottom>
-        {name}
-      </Typography>
+      {death ? (
+        <Stack
+          direction="column"
+          spacing={1}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Avatar sx={{ width: 50, height: 50, bgcolor: pink[500] }}>
+            <CloseIcon />
+          </Avatar>
+          <Typography variant="h7" component="div" gutterBottom>
+            {name}
+          </Typography>
+        </Stack>
+      ) : (
+        <Stack
+          direction="column"
+          spacing={1}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Avatar src="/broken-image.jpg" sx={{ width: 50, height: 50 }} />
+          <Typography variant="h7" component="div" gutterBottom>
+            {name}
+          </Typography>
+        </Stack>
+      )}
     </Stack>
   );
 }
