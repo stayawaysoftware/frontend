c97ef67e8ca30724991cef0da63d51b6d90349d9
Agustina Mollea <agustina.mollea@mi.unc.edu.ar>
2023-10-02 13:14:43 -0300

    SAS-146 the requested changes were made

--- src/components/GameComps/Deck.js
+++ src/components/GameComps/Deck.js
@@ -1,40 +1,14 @@
 import React from "react";
 import mazo from "../GameComps/Images/mazo.png";
-import { useState } from "react";
-//import { UserContext } from "../../contexts/UserContext";
 
 const Deck= () => {
-  const [selectedDeck, setSelectedDeck] = useState(null);
 
   const containerStyle = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
   };
-
-  const baseCardStyle = {
-    width: "35%",
-    height: "auto",
-    position: "relative",
-    userSelect: "none",
-    cursor: "pointer",
-    border: "none",
-    right: "30%",
-    bottom: "190px",
-  };
-
-  const highlightedDeckStyle = {
-    border: "1px groove",
-  };
-
-  const handleDeckClick = (Deck) => {
-    if (selectedDeck === Deck) {
-      setSelectedDeck(null);
-    } else {
-      setSelectedDeck(Deck);
-    }
-  };
-
+  
   return (
     <div>
       <div style={containerStyle}>
@@ -42,10 +16,15 @@ const Deck= () => {
           src={mazo}
           alt="mazo"
           style={{
-            ...baseCardStyle,
-            ...(selectedDeck === "mazo" ? highlightedDeckStyle : {}),
+            width: "35%",
+            height: "auto",
+            position: "relative",
+            userSelect: "none",
+            cursor: "pointer",
+            border: "none",
+            right: "30%",
+            bottom: "190px",
           }}
-          onClick={() => handleDeckClick("mazo")}
         />
       </div>
     </div>
