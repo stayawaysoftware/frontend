2563b892dbdfb7af676b89aeb2fb19745eef5259
gasmint <gastonbonfils@mi.unc.edu.ar>
2023-10-03 00:05:58 -0300

    SAS-183 added loading screen

--- src/views/Game/Game.js
+++ src/views/Game/Game.js
@@ -25,6 +25,7 @@ const Game = () => {
   const [gameData, setGameData] = useState(null);
   const [gamePlayers, setGamePlayers] = useState([]);
   const [currentTurn, setCurrentTurn] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   let players_example = [
     { id: 1, name: "Player 1", death: false },
@@ -43,12 +44,20 @@ const Game = () => {
   
   useEffect(() => {
     const getGameData = async () => {
-      const response = await axios.get(`http://localhost:8000/game/${gameId}`);
-      setGameData(response.data);
-      setGamePlayers(response.data.players);
-      setCurrentTurn(response.data.current_turn);
-    }
+      try {
+        const response = await axios.get(`http://localhost:8000/game/${gameId}`);
+        const data = response.data;
+        setGameData(data);
+        setGamePlayers(data.players);
+        setCurrentTurn(data.current_turn);
+        setLoading(false);
+      } catch (error) {
+        // Manejar errores de la solicitud
+        console.error("Error al obtener los datos del juego:", error);
+      }
+    };
     getGameData();
+    console.log("game data es",gameData);
 
     const interval = setInterval(getGameData, 20000);
 
@@ -56,7 +65,20 @@ const Game = () => {
 
   }, [gameId]);
 
+  const gameDataToTableData = (gameData) => {
+    let tableData = [];
+    gameData.players.forEach(player => {
+      tableData.push({
+        id: player.id,
+        name: player.name,
+        death: !(player.alive),
+      });
+    });
+    return tableData;
+  }
+
   const postitionToId = (position) => {
+    console.log("game players es", gamePlayers);
     let id = null;
     gamePlayers.forEach(player => {
       if (player.round_position === position) {
@@ -84,55 +106,63 @@ const Game = () => {
           backgroundSize: "cover",
         }}
       >
-        <GameTable players_example={players_example} currentTurn={currentTurn} />
-        <Box>
-          <Grid container spacing={2}>
-            <Grid item xs={6} md={20}>
-              <Deck />
-            </Grid>
-          </Grid>
-          <div
-            style={{
-              position: "relative",
-              left: "10%",
-              transform: "translate(16%, 0%)",
-              marginBottom: "-70%",
-            }}
-          ></div>
-        </Box>
-        <Box>
-          <Grid container spacing={2}>
-            <Grid item xs={6} md={20}>
-              <DescPile lastCard={lastCard} />
-            </Grid>
-          </Grid>
-          <div
-            style={{
-              position: "relative",
-              left: "10%",
-              transform: "translate(16%, 0%)",
-              marginBottom: "-70%",
-            }}
-          >
-          </div>
-        </Box>
-        <Box>  
-          <Grid container spacing={2}>
-            <Grid item xs={6} md={20}>
-              <Hand cardList={cardList} />
-            </Grid>
-          </Grid>
-          <div
-            style={{
-              position: "relative",
-              left: "50%",
-              transform: "translate(16%, 0%)",
-              marginTop: 0,
-            }}
-          >
-            <Buttons current_player={postitionToId(currentTurn)} />
-          </div>
-        </Box>
+        {loading ? (
+          // Mostrar el mensaje de carga si loading es true
+          <p>Loading...</p>
+        ) : (
+          // Mostrar los datos del juego si loading es false
+          <>
+            <GameTable players_example={gameDataToTableData(gameData)} currentTurn={currentTurn} />
+            <Box>
+              <Grid container spacing={2}>
+                <Grid item xs={6} md={20}>
+                  <Deck />
+                </Grid>
+              </Grid>
+              <div
+                style={{
+                  position: "relative",
+                  left: "10%",
+                  transform: "translate(16%, 0%)",
+                  marginBottom: "-70%",
+                }}
+              ></div>
+            </Box>
+            <Box>
+              <Grid container spacing={2}>
+                <Grid item xs={6} md={20}>
+                  <DescPile lastCard={lastCard} />
+                </Grid>
+              </Grid>
+              <div
+                style={{
+                  position: "relative",
+                  left: "10%",
+                  transform: "translate(16%, 0%)",
+                  marginBottom: "-70%",
+                }}
+              >
+              </div>
+            </Box>
+            <Box>  
+              <Grid container spacing={2}>
+                <Grid item xs={6} md={20}>
+                  <Hand cardList={cardList} />
+                </Grid>
+              </Grid>
+              <div
+                style={{
+                  position: "relative",
+                  left: "50%",
+                  transform: "translate(16%, 0%)",
+                  marginTop: 0,
+                }}
+              >
+                <Buttons current_player={postitionToId(currentTurn)} />
+              </div>
+            </Box>
+          </>
+        )}
       </div>
     </div>
   );
