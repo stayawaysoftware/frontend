2b1f4bab30cc1a3aa601229101869a2fbe3d69ec
gasmint <gastonbonfils@mi.unc.edu.ar>
2023-10-02 22:51:21 -0300

    SAS-162 Polling game status

--- src/views/Game/Game.js
+++ src/views/Game/Game.js
@@ -5,13 +5,13 @@ import GameTable from "../../components/GameTable/GameTable";
 import Deck from "../../components/GameComps/Deck";
 import DescPile from "../../components/GameComps/DescPile";
 
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
 import { UserContext } from "../../contexts/UserContext";
-import { useContext } from "react";
+import axios from "axios";
 
 const cardList = [200, 201, 202, 3, 202];
 const lastCard = 3;
@@ -21,6 +21,11 @@ const descPile = [];
 const Game = () => {
   const { gameId } = useParams();
   const { userid } = useContext(UserContext);
+  
+  //game data
+  const [gameData, setGameData] = useState(null);
+  const [gamePlayers, setGamePlayers] = useState([]);
+  const [currentTurn, setCurrentTurn] = useState(null);
 
   let players_example = [
     { id: 1, name: "Player 1", death: false },
@@ -36,6 +41,21 @@ const Game = () => {
     { id: 11, name: "Player 11", death: false },
     { id: 12, name: "Player 12", death: false },
   ];
+  
+  useEffect(() => {
+    const getGameData = async () => {
+      const response = await axios.get(`http://localhost:8000/game/${gameId}`);
+      setGameData(response.data);
+      setGamePlayers(response.data.players);
+      setCurrentTurn(response.data.current_turn);
+    }
+    getGameData();
+
+    const interval = setInterval(getGameData, 2000);
+
+    return () => clearInterval(interval);
+
+  }, [gameId]);
 
   return (
     <div>
@@ -54,7 +74,7 @@ const Game = () => {
           backgroundSize: "cover",
         }}
       >
-        <GameTable players_example={players_example} />
+        <GameTable players_example={gamePlayers} currentTurn={currentTurn} />
         <Box>
           <Grid container spacing={2}>
             <Grid item xs={6} md={20}>
