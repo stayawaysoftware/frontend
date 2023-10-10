d83cfe67bc0015880edfbd5f93eb3b43a2f78a7b
gasmint <gastonbonfils@mi.unc.edu.ar>
2023-10-09 21:58:23 -0300

    SAS-227 apitypes file

---
+++ src/utils/ApiTypes.js
@@ -0,0 +1,5 @@
+export const API_URL = "http://localhost:8000";
+
+export const API_URL_USERS = API_URL + "/users";
+export const API_URL_ROOMS = API_URL + "/rooms";
+export const API_URL_GAMES = API_URL + "/games";
